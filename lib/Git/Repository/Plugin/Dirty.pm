85c77bf5eb57f358fcce8bc99c2f7b06ae063c50
Daniel Muey <simplemood.com@gmail.com>
2018-01-18 08:11:38 -0600

    Add POD

--- lib/Git/Repository/Plugin/Dirty.pm
+++ lib/Git/Repository/Plugin/Dirty.pm
@@ -19,7 +19,8 @@ sub is_dirty {
 
 sub has_untracked {
     my ($git) = @_;
-    my @untracked = map { my $l = $_; $l =~ s/^\?\? //; $l } grep { m/^\?\? / } $git->run( "status", "-u", "-s", "--porcelain" );
+    my @untracked = map { my $l = $_; $l =~ s/^\?\? //; $l }
+      grep { m/^\?\? / } $git->run( "status", "-u", "-s", "--porcelain" );
     return @untracked;
 }
 
@@ -79,6 +80,8 @@ sub current_branch {
 
 __END__
 
+=encoding utf8
+
 =head1 NAME
 
 Git::Repository::Plugin::Dirty - methods to inspect the dirtiness of a git repository
@@ -111,70 +114,80 @@ This document describes Git::Repository::Plugin::Dirty version 0.01
 
 =head1 INTERFACE
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=head2 $git->is_dirty()
 
+Returns true if there are staged or unstaged changes.
+
+=head2 $git->is_dirty(untracked => 1)
+
+Also return true if there are untracked files.
+
+=head2 $git->has_unstaged_changes()
+
+Returns true if there are unstaged changes.
+
+=head2 $git->has_staged_changes()
+
+Returns true if there are staged changes.
+
+=head2 $git->has_untracked()
+
+Returns a list of untracked files.
+
+=head2 $git->diff_unstaged()
+
+Returns an array of lines making up the diff of unstaged changes.
+
+=head2 $git->diff_unstaged(\&output_handler)
+
+For each line of the unstaged changes diff this handler is called.
+
+It is passed the git object and the line.
+
+Returning true will continue to the next line.
+
+Returning false will stop the command.
+
+
+    $git->diff_staged(sub {
+        my ($git,$line) = @_;
+
+        if ($line =~ m/API_KEY:(.*)/) {
+            my $api_key = $1;
+            update_api_key_on_ci_boxes($api_key);
+            return; # since we found what we where looking for we can stop
+        }
+
+        return 1; # keep going
+    });
+
+=head2 $git->diff_staged()
+
+Returns an array of lines making up the diff of unstaged changes.
+
+=head2 $git->diff_staged(\&output_handler)
+
+Same as C<diff_staged()> but for staged changes.
+
+=head2 $git->current_branch()
+
+Returns the current branch. Useful for determining why a repo is dirty and what to do about it.
 
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
+Throws no warnings or errors of its own. Any errors will be from the L<Git::Repository> objects.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-
 Git::Repository::Plugin::Dirty requires no configuration files or environment variables.
-
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+L<Git::Repository::Plugin>
 
 =head1 INCOMPATIBILITIES AND LIMITATIONS
 
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
 None reported.
-
 
 =head1 BUGS AND FEATURES
 
